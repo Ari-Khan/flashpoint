6987ae4f61d83c666e20e5ab5ef8878a9ca7cfdc
Ari <ari.basil.khan@gmail.com>
2026-01-24 18:41:18 -0500

    Utility update.

--- src/utils/countryUtils.js
+++ src/utils/countryUtils.js
@@ -10,18 +10,23 @@ export function isoMatchesFeature(iso, feature) {
 }
 
 export function getNationByIso(iso, nations) {
-    return Object.values(nations).find((n) => n.iso2 === iso);
+    // Try to match by iso2 (2-letter) or by key (3-letter)
+    const byIso2 = Object.values(nations).find((n) => n.iso2 === iso);
+    if (byIso2) return byIso2;
+    if (nations[iso]) return nations[iso];
+    return undefined;
 }
 
-export function getColorByIso(iso, nations, defaultColor = "#ff6600") {
+export function getColorByIso(iso, nations, defaultColor = "#FF69B4") {
     const nation = getNationByIso(iso, nations);
     return nation?.defaultColor ?? defaultColor;
 }
 
 export function getCountryKey(feature) {
-    return (
-        feature.properties?.iso_a3 ||
-        feature.properties?.name ||
-        JSON.stringify(feature.geometry.coordinates[0][0])
-    );
+    const props = feature.properties || {};
+    const id = props.iso_a3 || props.iso_a2 || props.adm0_a3 || props.name;
+    
+    if (id) return id;
+
+    return `geom-${feature.geometry.type}-${JSON.stringify(feature.geometry.coordinates[0][0])}`;
 }
